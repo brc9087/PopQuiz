1548df3f4aedb39c9e2c8b16e97dac0bb7bd5a4c
Mona Lisa <bcardines811@gmail.com>
2021-02-14 14:19:07 -0500

    questionsupdatednba

---
+++ Main/assets/js/questions.js
@@ -0,0 +1,63 @@
+// list of all questions, choices, and answers
+var questions = [
+  {
+    title: "Who is the first 8th seeded team to make the NBA finals",
+    choices: [
+      "New York Knicks",
+      "LA Lakers",
+      "Dallas Mavericks",
+      "Miami Heat"
+    ],
+    answer: "New York Knicks"
+  },
+  {
+    title: "Who is the the player that won the most NBA Finals MVP's",
+    choices: [
+      "Allan Houston",
+      "Kobe Bryant",
+      "Michael Jordan",
+      "Lebron James"
+    ],
+    answer: "Michael Jordan"
+  },
+  {
+    title: "What NBA team drafted Kobe Bryant",
+    choices: [
+      "Chicago Bulls",
+      "LA Lakers",
+      "San Antonio Spurs",
+      "Charlotte Hornets"
+    ],
+    answer: "Charlotte Hornets"
+  },
+  {
+    title: "Who is the the player that won the most NBA Finals MVP's",
+    choices: [
+      "Allan Houston",
+      "Kobe Bryant",
+      "Michael Jordan",
+      "Lebron James"
+    ],
+    answer: "Michael Jordan"
+  },
+  {
+    title: "Which player has the highest career free throw percentage",
+    choices: [
+      "Stephen Curry",
+      "Steve Nash",
+      "Michael Jordan",
+      "Dirk Nowitzki"
+    ],
+    answer: "Steve Nash"
+  },
+  {
+    title: "What team has had the most Hall of Fame players on roster",
+    choices: [
+      "Boston Celtics",
+      "New York Knicks",
+      "Chicago Bulls",
+      "LA Lakers"
+    ],
+    answer: "Boston Celtics"
+  },
+];
--- README.md
+++ README.md
@@ -1,8 +1,13 @@
-# EmployeeReactDirectory
+# Code Quiz
 
-* This is an employee directory app with React. As a user, I want to be able to view my entire employee directory at once so that I have quick access to their information.
-
-* An employee or manager would benefit greatly from being able to view non-sensitive data about other employees. It would be particularly helpful to be able to filter employees by name.
+* Once the user starts the quiz, present the first question and start the coutdown timer.
+When the user selects an answer, pause the quiz timer and let the user know if the answer right/wrong.
+If the user is wrong, subtract penalty time from the clock.
+After 2 seconds, proceed to the next question and resume the countdown timer.
+If the countdown timer reaches 0 the quiz is over.
+If the user answers all the questions the quiz is over.
+The user's score and initials are then saved after which the high scores will be displayed.
+Also provide a link straight to viewing the high scores page.
 
 ## Usage / Directions
 * Click the link to find the Employee https://brc9087.github.io/myapp/
